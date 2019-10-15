34972899db623a5c03cea607da507e6f83346ba0
Matthieu Muschinowski <matthieu.muschinowski@gmail.com>
2019-10-15 19:06:37 +0200

    Re-arranged more logically, added a command publisher & comments

--- src/trajectory-control_node.cpp
+++ src/trajectory-control_node.cpp
@@ -10,27 +10,45 @@
 
 int main(int argc, char *argv[])
 {
-	ros::init(argc, argv, "trajectory_control_node");
+  ros::init(argc, argv, "trajectory_control_node");
   ros::NodeHandle n;
   ros::Rate r = 10;
+  float dt = r.expectedCycleTime().toSec();
 
-  float dt = .1;
 
-  DroneStates state_current, state_predicted, state_target;
+  // Declare all publishers & subscribers
+  ros::Publisher attitudeCommand_pub = n.advertise<geometry_msgs::Vector3>("attitudeCommand", 10);
+
+  DroneStates measured_state, state_predicted, state_target;
   FullStatesFeedback fsf;
   KinematicTransform kT;
   StatesEstimator estimator;
 
-  geometry_msgs::Vector3 cmd, cmdkT, eulerAng;
+  bool useEKF = false; //TODO: make this a dynamic parameter.
+
+  geometry_msgs::Vector3 accelerationCommand, attitudeCommand, eulerAng;
 
   while (n.ok())
   {
     ROS_INFO("******************* This is a test *******************");
 
-    cmd = fsf.process(dt, state_current, state_target);
-    cmdkT = kT.process(cmd, eulerAng);
-    state_predicted = estimator.process(dt, state_current.getVectPos(), state_predicted, cmd);
-    state_current = state_predicted;
+    //Get actual dt, not expected one
+  	dt = r.cycleTime().toSec();
+
+    measured_state = state_predicted; // Should be measured state, so either position from MOCAP or from px4's EKF.
+    eulerAng = eulerAng; // Should be measured angles from attitude estimation 
+
+  	//Get the estimated state from measures and previous estimation & command
+    state_predicted = estimator.process(dt, measured_state.getVectPos(), state_predicted, accelerationCommand);
+    if(useEKF) state_predicted = measured_state; 
+
+    //Compute full state feedback control
+    accelerationCommand = fsf.process(dt, state_predicted, state_target);
+
+    //Generate (roll, pitch, thrust) command
+    attitudeCommand = kT.process(accelerationCommand, eulerAng);
+
+	attitudeCommand_pub.publish(attitudeCommand);
 
     ros::spinOnce();
     r.sleep();
