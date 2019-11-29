24533a7d5604107a25200f89056934868b04fc5c
Matheus Bruhns Bastos <matbbastos@gmail.com>
2019-11-29 16:27:00 +0100

    Add python script to test plugin

---
+++ plugin_test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python2
+
+# import math
+# import numpy as np
+# from scipy import signal
+# from matplotlib import pyplot as plt
+# from mpl_toolkits.mplot3d import Axes3D
+# from timeit import default_timer as time
+
+import rospy
+from std_msgs.msg import String, Header
+from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
+# from package_name.msgs import message_name
+from mavlink_sitl_gazebo.msgs 
