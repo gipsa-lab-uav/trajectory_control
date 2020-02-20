4ef15f19055b9f150b0b7b4a7efbff2d593fdae8
SaAPro <alexandre_sa@live.fr>
2020-02-20 11:19:21 +0100

    clamp thrust before computing pitch and roll & add some comments

--- src/trajectory_control/kinematicTransform.cpp
+++ src/trajectory_control/kinematicTransform.cpp
@@ -16,17 +16,16 @@ KTParameters::KTParameters(float Komp, float m, float angleMax, float maxVertica
     maxVerticalAcceleration = maxVerticalAcc;
 }
 
-KTParameters::~KTParameters(){}
+KTParameters::~KTParameters() {}
 
-KinematicTransform::KinematicTransform(){}
-KinematicTransform::~KinematicTransform(){}
+KinematicTransform::KinematicTransform() {}
+KinematicTransform::~KinematicTransform() {}
 
 geometry_msgs::Vector3 KinematicTransform::process(geometry_msgs::Vector3 accelerations, geometry_msgs::Vector3 droneEulerAngles)
 {
     // outputCmd(x,y,z) <=> (roll, pitch, thrust)
     geometry_msgs::Vector3 outputCmd;
     float G = 9.81f;
-
 
     float Rad2Deg = 180.0f / 3.14f;
 
@@ -35,7 +34,14 @@ geometry_msgs::Vector3 KinematicTransform::process(geometry_msgs::Vector3 accele
     float cosYaw = cos(droneEulerAngles.z);
     float sinYaw = sin(droneEulerAngles.z);
 
+    float maxAngleRad = param.maxAngle / Rad2Deg;
+
+    // float accelerationZ = KinematicTransform::clamp(accelerations.z, -param.maxVerticalAcceleration, param.maxVerticalAcceleration);
+
     float thrust = param.mass * (accelerations.z + G) / (cosRoll * cosPitch);
+
+    // Clamp thrust to max value
+    thrust = KinematicTransform::clamp(thrust, 0.0f, param.mass * (param.maxVerticalAcceleration + G));
 
     //if we are going in freefall, we will do it horizontally, it will be easier to apply thrust to stabilize the drone when wanted
     if (thrust <= 0.2f * param.mass)
@@ -49,34 +55,39 @@ geometry_msgs::Vector3 KinematicTransform::process(geometry_msgs::Vector3 accele
         // roll = std::Asin(mass / thrust * ( cmdDrone.translation.x * sinYaw - cmdDrone.translation.y * cosYaw ) );
         // pitch = std::Asin(mass / thrust * ( cmdDrone.translation.x * cosYaw + cmdDrone.translation.y * sinYaw) ) );
         // Yaw shouldn't change, so we keep the cos&sin as is, but we have x_reg = z_unity ; y_reg = -x_unity ; z_reg = y_unity
-        outputCmd.x = asin(KinematicTransform::clamp(param.mass * (accelerations.x * sinYaw - accelerations.y * cosYaw) / thrust, -1.0f,1.0f));
-        outputCmd.y = asin(KinematicTransform::clamp(param.mass * (accelerations.x * cosYaw + accelerations.y * sinYaw) / thrust, -1.0f,1.0f));
+        // The clamp is to ensure the quantity inside the asin to be between [-1, 1]
+        outputCmd.x = asin(KinematicTransform::clamp(param.mass * (accelerations.x * sinYaw - accelerations.y * cosYaw) / thrust, -1.0f, 1.0f));
+        outputCmd.y = asin(KinematicTransform::clamp(param.mass * (accelerations.x * cosYaw + accelerations.y * sinYaw) / thrust, -1.0f, 1.0f));
     }
 
-    // Clamp to max values
-    outputCmd.x = KinematicTransform::clamp((outputCmd.x * Rad2Deg), -param.maxAngle, param.maxAngle);
-    outputCmd.y = KinematicTransform::clamp((outputCmd.y * Rad2Deg), -param.maxAngle, param.maxAngle);
-    outputCmd.z = KinematicTransform::clamp(thrust, 0.0f, (G + param.maxVerticalAcceleration) * param.mass);
+    // Clamp roll and pitch to maximum angle
+    outputCmd.x = KinematicTransform::clamp(outputCmd.x, -maxAngleRad, maxAngleRad);
+    outputCmd.y = KinematicTransform::clamp(outputCmd.y, -maxAngleRad, maxAngleRad);
 
     // Map outputs between -1 and 1.
-    outputCmd.x = outputCmd.x / param.maxAngle;
-    outputCmd.y = outputCmd.y / param.maxAngle;
+    // outputCmd.x = outputCmd.x / param.maxAngle;
+    // outputCmd.y = outputCmd.y / param.maxAngle;
     //outputCmd.x = (outputCmd.x + param.maxAngle) / (2.0f*param.maxAngle);
     //outputCmd.y = (outputCmd.y + param.maxAngle) / (2.0f*param.maxAngle);
-    outputCmd.z = outputCmd.z * param.hoverCompensation / (param.mass * G);
+
+    outputCmd.z = thrust * param.hoverCompensation / (param.mass * G);
+    // outputCmd.z = param.hoverCompensation + accelerationZ / param.maxVerticalAcceleration;
 
     return outputCmd;
 }
 
 float KinematicTransform::clamp(float value, float lowBound, float upperBound)
 {
-    if (value < lowBound){
+    if (value < lowBound)
+    {
         return lowBound;
     }
-    else if (value > upperBound){
+    else if (value > upperBound)
+    {
         return upperBound;
     }
-    else{
+    else
+    {
         return value;
     }
 }
